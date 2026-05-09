905d444c2c7c5abc9c3654a80b404e5ee82a710e
himani-sikarwar <himanisikarwarhs@gmail.com>
2026-05-09 18:43:34 +0000

    fixing issue with test js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   const element = document.getElementById('animated-element');
+  console.log('Animation script loaded');
   element.classList.add('animate');
 });
